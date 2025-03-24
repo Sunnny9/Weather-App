b2637f11b7b9239df946fbd16ed8feb1041b9bb3
Sunny <letsmailsunny9@gmail.com>
2025-03-25 01:07:44 +0530

    Fix: Updated TypeScript types in page.tsx

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -2,20 +2,19 @@ import WeatherForm from "@/components/weather/weatherForm";
 import WeatherList from "@/components/weather/weatherList";
 import React from "react";
 
-interface SearchParams{
-    searchParams: {
-        cityName: string;
-    };
+interface PageProps {
+  searchParams: { [key: string]: string | undefined };
 }
 
+const Weather = ({ searchParams }: PageProps) => {
+  const city = searchParams.cityName || "Unknown"; // Fallback if cityName is undefined
 
-const Weather = async ({ searchParams }: SearchParams) => {
-    const city = searchParams.cityName;
   return (
-    <div className="flex justify-center h-screen bg-cover" style={{ backgroundImage: "url('/images/weather.jpg')"} }>
-    
-        
-      <div className="flex flex-col mt-50 justify-center items-center  gap-6 border-5 rounded-2xl bg-white/30 backdrop-blur w-[400px] h-[200px] shadow-lg shadow-gray-500">
+    <div 
+      className="flex justify-center h-screen bg-cover"
+      style={{ backgroundImage: "url('/images/weather.jpg')" }}
+    >
+      <div className="flex flex-col mt-50 justify-center items-center gap-6 border-5 rounded-2xl bg-white/30 backdrop-blur w-[400px] h-[200px] shadow-lg shadow-gray-500">
         <header className="text-4xl font-semibold"> Weather App </header>
         <WeatherForm />
         <WeatherList city={city} />
