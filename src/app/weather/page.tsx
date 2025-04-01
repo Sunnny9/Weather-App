a651f9ecbb9fdf239bcda2e85e7c6597a88ea146
Sunny <letsmailsunny9@gmail.com>
2025-04-01 15:17:36 +0530

    Change file route

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -10,9 +10,11 @@ const Weather = async ({ searchParams }) => {
       className="flex justify-center h-screen bg-cover"
       style={{ backgroundImage: "url('/images/weather.jpg')" }}
     >
-      <div className="flex flex-col mt-50 justify-center items-center  gap-6
+      <div
+        className="flex flex-col mt-50 justify-center items-center  gap-6
        border-5 rounded-2xl bg-white/30 backdrop-blur w-[400px] h-[200px] 
-       shadow-lg shadow-gray-500">
+       shadow-lg shadow-gray-500"
+      >
         <header className="text-4xl font-semibold"> Weather App </header>
         <WeatherForm />
         <WeatherList city={city} />
