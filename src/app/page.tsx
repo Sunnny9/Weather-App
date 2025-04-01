51dd8af7c41a4ad9e54f1322b323fb2e6a3744b9
Sunny <letsmailsunny9@gmail.com>
2025-04-01 15:24:32 +0530

    type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,50 @@
+// import WeatherForm from "@/components/weather/weatherForm";
+// import WeatherList from "@/components/weather/weatherList";
+// import React from "react";
+
+// const Weather = async ({ searchParams }) => {
+//   let city = (await searchParams).cityName;
+//   city = city ? city.charAt(0).toUpperCase() + city.slice(1) : "";
+//   return (
+//     <div
+//       className="flex justify-center h-screen bg-cover"
+//       style={{ backgroundImage: "url('/images/weather.jpg')" }}
+//     >
+//       <div
+//         className="flex flex-col mt-50 justify-center items-center  gap-6
+//        border-5 rounded-2xl bg-white/30 backdrop-blur w-[400px] h-[200px] 
+//        shadow-lg shadow-gray-500"
+//       >
+//         <header className="text-4xl font-semibold"> Weather App </header>
+//         <WeatherForm />
+//         <WeatherList city={city} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Weather;
+
+
 import WeatherForm from "@/components/weather/weatherForm";
 import WeatherList from "@/components/weather/weatherList";
 import React from "react";
 
-const Weather = async ({ searchParams }) => {
-  let city = (await searchParams).cityName;
+interface WeatherProps {
+  searchParams: Record<string, string | undefined>;
+}
+
+const Weather: React.FC<WeatherProps> = async ({ searchParams }) => {
+  let city = searchParams.cityName || "";
   city = city ? city.charAt(0).toUpperCase() + city.slice(1) : "";
+
   return (
     <div
       className="flex justify-center h-screen bg-cover"
       style={{ backgroundImage: "url('/images/weather.jpg')" }}
     >
       <div
-        className="flex flex-col mt-50 justify-center items-center  gap-6
+        className="flex flex-col mt-50 justify-center items-center gap-6
        border-5 rounded-2xl bg-white/30 backdrop-blur w-[400px] h-[200px] 
        shadow-lg shadow-gray-500"
       >
